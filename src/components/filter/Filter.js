499e5c0a45da7b14bda88af4c65fe1db3dadceda
Tyler Schmidt <schmidt197@gmail.com>
2018-10-24 11:21:40 -0700

    adding file structue for Filter and Map columns

---
+++ src/components/filter/Filter.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './styles.css';
+
+class Filter extends Component {
+  render() {
+    return (
+      <div className='filter-component'>
+      <div>
+        <h1>FILTER AREA</h1>
+        <ul>
+        <li>Locations</li>
+        <li>Locations</li>
+        <li>Locations</li>
+        </ul>
+      </div>
+    </div>);
+  }
+}
+
+export default Filter;
---
+++ src/components/map/Map.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './styles.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+      <div>
+        <h1>MAPS AREA</h1>
+      </div>
+    </div>);
+  }
+}
+
+export default Map;
