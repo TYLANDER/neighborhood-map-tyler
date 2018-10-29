3bdeaa5106e0c8d55cac0f054a3c8b7c7809eacd
Tyler Schmidt <schmidt197@gmail.com>
2018-10-29 11:57:07 -0700

    Added filtering logic

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -3,14 +3,30 @@ import './styles.css';
 import locations from '../../locations'
 
 class Filter extends Component {
+  state = {
+    locations,
+    items:locations
+  }
+
+  filterList = (event) => {
+    var updatedList = this.state.locations;
+    updatedList = updatedList.filter(function(item){
+      return item.name.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({items: updatedList});
+  }
+
   render() {
-    const items = locations.map(location => {
+    console.log(this.state)
+    const items = this.state.items.map(location => {
       return <li key={location.name}>{location.name}</li>
     })
     return (
       <div className='filter-component'>
       <div>
         <h1>FILTER AREA</h1>
+        <input type='text' placeholder='filter dim sum' onChange={this.filterList}/>
         <ul>
         {items}
         </ul>
