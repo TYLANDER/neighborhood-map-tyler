8f696a40a25d2f062938fbe36203f89a42b324d1
Tyler Schmidt <schmidt197@gmail.com>
2018-10-29 10:51:03 -0700

    Google Map Rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Filter from './components/filter/Filter'
 import Map from './components/map/Map'
 
+/* NEXT STEPS
+1. get map rendering in the maps area.
+2. Get 1 pin to render on the map.
+3. Loop through an array of Pins.
+4. Render the pin's var in the filter area to show all locations.
+5. Filtering. */
+
 class App extends Component {
   render() {
     return (<div className="App">
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
 import './styles.css';
+import GoogleMapReact from 'google-map-react';
+
+const googleMapKey = 'AIzaSyB1jeZYx7O8UR-TFm5K1e3ZBdkd5cEWcsw';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.7775,
+      lng: -122.4333
+    },
+    zoom: 11
+  };
+
   render() {
     return (
-      <div>
-      <div>
-        <h1>MAPS AREA</h1>
-      </div>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: googleMapKey }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.7775}
+            lng={-122.4333}
+            text={'Alamo Square Park'}
+          />
+        </GoogleMapReact>
     </div>);
   }
 }
