2a73cdd039c32a0f1bfe295126331861d7e6cbc4
Tyler Schmidt <schmidt197@gmail.com>
2018-10-30 11:54:37 -0700

    added click event lister to marker

--- src/components/marker/Marker.js
+++ src/components/marker/Marker.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types'
 import './styles.css'
 
 
+
+
 export class Marker extends React.Component {
+  handleClick = () => {
+    console.log('Pin clicked')
+  }
   render() {
-    return <div className='pin'></div>
+    return <div className='pin' onClick={this.handleClick}></div>
   }
 }
 
